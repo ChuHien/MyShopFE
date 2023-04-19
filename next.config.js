773a7a80ace4bddec0a977b402df385477dd41dc
ChuHien <chuhien4220na@gmail.com>
2023-04-19 11:25:46 +0700

    config use scss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  cssModules: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    })
+
+    return config
+  }
 }
 
 module.exports = nextConfig
